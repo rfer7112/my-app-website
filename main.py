9ba1a2496e67af4a73a78cadd0fdf448865db468
REDBLADE86 <Russell.workid@gmail.com>
2024-06-20 06:50:05 +0200

    Day 21: Added description to main.py

--- main.py
+++ main.py
@@ -10,12 +10,15 @@ with col1:
 
 with col2:
     st.title('Russel Fernandes')
-    content = """
-    This is me, i am still learning Python, below you will find my learning apps
+    about_me = """
+    This is me, i am still learning Python.
     I am a DLP specialist with a background in IT. 
     However, Python is the first programming language i am learning online and it is really cool"""
-    st.info(content)
+    st.info(about_me)
 
+text1 = """
+Below you can find some of the apps i have built in Python. Feel free to contact me"""
+st.write(text1)
 # with open('data.csv', 'r') as file:
 #     data = file.read()
 #
