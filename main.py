57f0ac28fbf5a6c95cca681e815e94a05f740611
REDBLADE86 <Russell.workid@gmail.com>
2024-06-20 07:16:35 +0200

    Day 23: Added app images and source code link

--- main.py
+++ main.py
@@ -28,7 +28,13 @@ df = pandas.read_csv('data.csv', sep=';')
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[Source Code]({row['url']})")
